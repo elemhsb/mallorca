bb43bb0834183fac145c218cbc0b2eb4ab903883
0lri <Oliver.Riesener@hs-bremen.de>
2013-02-13 16:27:26 +0100

    fixes to openocd.sh

--- conf/system/HB/bin/openocd.sh
+++ conf/system/HB/bin/openocd.sh
@@ -1,34 +1,44 @@
 #!/bin/sh
 
-#PATH=/opt/local/bin:$PATH:.
+[ "$PAPARAZZI_HOME" = "" ] && printf "$0: failed!   PAPARAZZI_HOME not set\n\n" && exit 1
+
 PATH=/usr/bin:/bin
 
 cd $PAPARAZZI_HOME
 
-OCDCONFDIR=conf/system/HB
+# Configuration sources
+OCDSCRIPTS=/usr/share/openocd/scripts
 
+# Configuration destination relativ to PAPARAZZI_HOME
+OCDCONFDIR=var
+
+# functions
 inline_edit()
 {
     # remove Warning about 12Mhz, we have 12Mhz.
     sed -i 's/	echo/	# &/' "$1"
 }
 
+
+# Olimex ARM-USB-OCD config
 CONFIG=$OCDCONFDIR/openocd-arm-usb-ocd.cfg
 if [ ! -s $CONFIG ] ;  then
 	cat \
-	/opt/local/share/openocd/scripts/interface/olimex-arm-usb-ocd.cfg \
-	/opt/local/share/openocd/scripts/target/lpc2148.cfg > $CONFIG
+	    $OCDSCRIPTS/interface/olimex-arm-usb-ocd.cfg \
+	    $OCDSCRIPTS/target/lpc2148.cfg > $CONFIG
 	inline_edit $CONFIG
 fi
 
+# Olimex ARM-USB-OCD-H config
 CONFIG_H=$OCDCONFDIR/openocd-arm-usb-ocd-h.cfg
 if [ ! -s $CONFIG_H ] ;  then
 	cat \
-	/opt/local/share/openocd/scripts/interface/olimex-arm-usb-ocd-h.cfg \
-	/opt/local/share/openocd/scripts/target/lpc2148.cfg > $CONFIG_H
+	    $OCDSCRIPTS/interface/olimex-arm-usb-ocd-h.cfg \
+	    $OCDSCRIPTS/target/lpc2148.cfg > $CONFIG_H
 	inline_edit $CONFIG_H
 fi
 
+# Symlink to User defined configuration
 OPENOCD_CFG=$PAPARAZZI_HOME/openocd.cfg
 if [ ! -h $OPENOCD_CFG ] ; then
 	printf "Which version ARM-USB-OCD or ARM-USB-OCD-H as default ?\n"
@@ -41,8 +51,9 @@ if [ ! -h $OPENOCD_CFG ] ; then
 	fi
 fi
 
+# Stop all running openocd
 killall openocd >/dev/null 2>&1 
 sleep 0.5
 
-exec openocd "$@" > openocd.log 2>&1 &
-sleep 0.5
+# Start new OPENOCD
+exec openocd -f $OPENOCD_CFG "$@" > openocd.log 2>&1 &
